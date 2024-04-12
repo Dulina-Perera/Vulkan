cb4b32244cc21b5f29bb0397bb11e6f70156b984
Dulina-Perera <dulina.perera@outlook.com>
2024-04-12 09:54:39 +0530

    Refined the shell script for compiling the shaders.

--- Vulkan-Engine/compile.sh
+++ Vulkan-Engine/compile.sh
@@ -1,2 +1,14 @@
-/usr/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-/usr/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+#!/bin/bash
+
+glslc_path=$(which glslc)
+
+if [ -z "$glslc_path" ]; then
+    echo "glslc not found. Please install it using your package manager."
+    exit 1
+fi
+
+mkdir shaders/build
+
+for file in shaders/src/*.vert shaders/src/*.frag; do
+    $glslc_path $file -o shaders/build/$(basename $file).spv
+done
