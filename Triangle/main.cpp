35007235751fc68682e7daafc3ff715f653ae79c
Dulina-Perera <dulina.perera@outlook.com>
2024-04-17 18:51:32 +0530

    Learnt how to create an instance.

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -1,9 +1,11 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <stdexcept>
 #include <cstdlib>
+#include <iostream>
+#include <set>
+#include <stdexcept>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -19,6 +21,7 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();
@@ -29,8 +32,67 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
-    void initVulkan() {
+    std::vector<VkExtensionProperties> getAvailableExtensions() {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        return extensions;
+    }
+
+    bool checkRequiredExtensionsAvailable(uint32_t glfwExtensionCount, const char** glfwExtensions) {
+        std::vector<VkExtensionProperties> availableExtensions = getAvailableExtensions();
+
+        std::set<std::string> availableExtensionNames;
+        for (const auto& extension : availableExtensions) {
+            availableExtensionNames.insert(extension.extensionName);
+        }
+
+        for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+            if (availableExtensionNames.find(glfwExtensions[i]) == availableExtensionNames.end()) {
+                std::cerr << "Required extension '" << glfwExtensions[i] << "' is not available.\n";
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        if (!checkRequiredExtensionsAvailable(glfwExtensionCount, glfwExtensions)) {
+            throw std::runtime_error("Required extensions are not available!");
+        }
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create instance!");
+        }
+    }
+
+    void initVulkan() {
+        createInstance();
     }
 
     void loop() {
@@ -40,8 +102,9 @@ private:
     }
 
     void cleanup() {
-        glfwDestroyWindow(window);
+        vkDestroyInstance(instance, nullptr);
 
+        glfwDestroyWindow(window);
         glfwTerminate();
     }
 };
