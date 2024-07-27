aa96395a4a73f7c87c3ed19a7b678cc1adefbb41
Pyrokryos <dulina.perera@outlook.com>
2024-07-27 23:14:12 +0530

    Defined a function to choose the preferred surface format for the swap chain.

--- Self-Learning/Triangle/main.cc
+++ Self-Learning/Triangle/main.cc
@@ -304,14 +304,15 @@ private:
 		}
 
 		if (
-			deviceFeatures.geometryShader &&
-			findQueueFamilies(device).isComplete() &&
-			areDeviceExtensionsSupported(device) &&
-			swapChainAdequate)
+				deviceFeatures.geometryShader &&
+				findQueueFamilies(device).isComplete() &&
+				areDeviceExtensionsSupported(device) &&
+				swapChainAdequate)
 		{
 			return score;
 		}
-		else {
+		else
+		{
 			return 0;
 		}
 	}
@@ -354,6 +355,19 @@ private:
 		}
 
 		return details;
+	}
+
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats)
+	{
+		for (const VkSurfaceFormatKHR &availableFormat : availableFormats)
+		{
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+			{
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];
 	}
 
 	void createInstance()
